7ef4c842bf1b5ada5bb2edaf806e5d5f873b4313
devansh-bolsure <88781820+devansh-bolsure@users.noreply.github.com>
2023-10-13 21:12:29 +0530

    Add files via upload

---
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,18 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController 
+{
+	@GetMapping("/home")
+	public String home()
+	{
+		System.out.println("This is home URL");
+		return "index";
+	}
+    
+   
+}
+
