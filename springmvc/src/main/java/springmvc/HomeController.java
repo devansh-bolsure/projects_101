0b6449c162ce1c6f6ad9a34478f8e14df283745c
devansh-bolsure <88781820+devansh-bolsure@users.noreply.github.com>
2023-10-16 15:53:20 +0530

    Add files via upload

---
+++ springmvc/src/main/java/springmvc/HomeController.java
@@ -0,0 +1,66 @@
+package springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Controller
+@RequestMapping("/first")
+public class HomeController 
+{
+	@RequestMapping(value="/home")
+	public String home(Model model)
+	{
+		System.out.println("this is home url");
+		model.addAttribute("name", "Akash");
+		model.addAttribute("id", 12345);
+		
+		List<String> friends=new ArrayList<String>();
+		friends.add("Abhishek");
+		friends.add("Krushna");
+		friends.add("Tejas");
+		model.addAttribute("f", friends);
+		return "index";
+	}
+	
+	@RequestMapping(value="about")
+	public String about()
+	{
+		System.out.println("this is about url");
+		return "about";
+	}
+	
+	@RequestMapping("/help")
+	public ModelAndView help()
+	{
+		System.out.println("this is help url");
+		
+		//creating modelAndView object
+		ModelAndView modelAndView=new ModelAndView();
+		
+		//setting Data
+		modelAndView.addObject("name","Zoro");
+		modelAndView.addObject("id",666);
+		LocalDateTime now = LocalDateTime.now();
+		modelAndView.addObject("time",now);
+		List<Integer> numbers=new ArrayList<Integer>();
+		numbers.add(476);
+		numbers.add(9862);
+		numbers.add(923);
+		modelAndView.addObject("numbers",numbers);
+//		setting view name(was working fine without it)
+		modelAndView.setViewName("help");
+		return modelAndView;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ springmvc/target/springmvc/WEB-INF/views/help.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import= "java.time.LocalDateTime" %>
+<%@ page isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %><html>
+<head>
+<meta charset="ISO-8859-1">
+<title> Help page</title>
+</head>
+<body>
+	<h1>This is help page</h1>
+	<%
+// 		String name=(String)request.getAttribute("name");
+// 		Integer id=(Integer)request.getAttribute("id");
+// 		LocalDateTime time=(LocalDateTime)request.getAttribute("time");
+	%>
+	<h1>Hello My name is:
+			${name }
+	</h1>
+	
+	<h1>My Lucky number is:
+		${id }
+	</h1>
+	
+	<h1>My Local Time is:
+		${time }
+	</h1>
+
+	
+	
+	<c:forEach var="item" items="${numbers}" >
+		<h1> ${item} </h1>
+	</c:forEach>
+	
+</body>
+</html>
